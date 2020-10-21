c7d7c8d822872ff44325f0fa89c6be661fbfd3a1
f0nkey <mystery@gmail.com>
2020-10-20 20:56:26 -0500

    Add prelim test appendHashes

--- main_test.go
+++ main_test.go
@@ -2,8 +2,113 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
+	"os"
 	"testing"
 )
+
+func TestAppendHashes(t *testing.T) {
+	defer func() {
+		//err := os.RemoveAll("./test")
+		//if err != nil {
+		//	t.Fatal(err)
+		//}
+	}()
+	createTestDirFiles(t)
+	appendHashes()
+}
+
+func createTestDirFiles(t *testing.T) {
+	err := os.RemoveAll("./test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.MkdirAll("./test/assets", 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("./test/cool.js", []byte(`console.log("cool and good")`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/cooler.js", []byte(`console.log("cooler and good")`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/lame.js", []byte(`console.log("lame and good")`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/styles.css", []byte(`h1{font-size:12px;}`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("./test/assets/pretty-styles.css", []byte(`h2{font-size:20px;}`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/assets/ugly-styles.css", []byte(`h3{font-size:30px;}`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/assets/big.js", []byte(`console.log("big and good")`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("./test/assets/bloat.js", []byte(`console.log("bloat and good")`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("./test/index.html", []byte(`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Title</title>
+			<link rel="stylesheet" href="styles.css">
+			<link rel="stylesheet" href="more-styles.css">
+			<script src="cool.js"></script>
+			<script src="./cooler.js"></script>
+		
+		
+			<script src="assets/bloat.js"></script>
+			<script src="./assets/big.js"></script>
+			<link rel="stylesheet" href="assets/pretty-styles.css">
+			<link rel="stylesheet" href="./assets/ugly-styles.css">
+		</head>
+		<body>
+			<h1>Fear</h1>
+			<h2>The</h2>
+			<h3>Cache Clobber</h3>
+		</body>
+		</html>`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("./test/assets/markup.html", []byte(`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Title</title>
+			<script src="../lame.js"></script>
+			<script src="../cooler.js"></script>
+			<script src="../cool.js"></script>
+		</head>
+		<body>
+			<h1>Fear</h1>
+			<h2>The</h2>
+			<h3>Cache Clobber</h3>
+		</body>
+		</html>`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestHrefFilePath(t *testing.T) {
 	nonErrorTests := []struct {
